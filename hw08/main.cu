cae45fd26f70aabf667602f5ceb972416ceabe9f
lymiao888 <3041105277@qq.com>
2024-05-26 16:36:49 +0800

    finish the basic requirement

--- hw08/main.cu
+++ hw08/main.cu
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cuda_device_runtime_api.h>
 #include <cuda_runtime.h>
 #include "CudaAllocator.h"
 #include "helper_cuda.h"
@@ -8,18 +9,19 @@
 
 // 这是基于“边角料法”的，请把他改成基于“网格跨步循环”的：10 分
 __global__ void fill_sin(int *arr, int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
-    arr[i] = sinf(i);
+    for(int i = threadIdx.x; i < n; i += blockDim.x) {
+        arr[i] = sinf(i);
+    }
 }
 
 __global__ void filter_positive(int *counter, int *res, int const *arr, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
+    if (i > n) return;
     if (arr[i] >= 0) {
         // 这里有什么问题？请改正：10 分
-        int loc = *counter;
-        *counter += 1;
+        // int loc = *counter;
+        // *counter += 1;
+        int loc = atomicAdd(counter, 1); // 改成了原子操作
         res[loc] = n;
     }
 }
@@ -28,27 +30,32 @@ int main() {
     constexpr int n = 1<<24;
     std::vector<int, CudaAllocator<int>> arr(n);
     std::vector<int, CudaAllocator<int>> res(n);
-    std::vector<int, CudaAllocator<int>> counter(1);
+    int *pcounter;
+    int counter;
+    checkCudaErrors(cudaMalloc(&pcounter, sizeof(int)));
 
     // fill_sin 改成“网格跨步循环”以后，这里三重尖括号里的参数如何调整？10 分
-    fill_sin<<<n / 1024, 1024>>>(arr.data(), n);
+    int nthreads = 128;
+    int nblocks = (n + nthreads + 1) / nthreads;
+    fill_sin<<<nblocks, nthreads>>>(arr.data(), n);
 
     // 这里的“边角料法”对于不是 1024 整数倍的 n 会出错，为什么？请修复：10 分
-    filter_positive<<<n / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
+    filter_positive<<<n / 1024, 1024>>>(pcounter, res.data(), arr.data(), n);
 
     // 这里 CPU 访问数据前漏了一步什么操作？请补上：10 分
-
-    if (counter[0] <= n / 50) {
-        printf("Result too short! %d <= %d\n", counter[0], n / 50);
+    checkCudaErrors(cudaMemcpy(&counter, pcounter, sizeof(int), cudaMemcpyDeviceToHost));
+    if (counter <= n / 50) {
+        printf("Result too short! %d <= %d\n", counter, n / 50);
         return -1;
     }
-    for (int i = 0; i < counter[0]; i++) {
+    for (int i = 0; i < counter; i++) {
         if (res[i] < 0) {
             printf("Wrong At %d: %f < 0\n", i, res[i]);
-            return -1;  // 突然想起了ICPC有一年队名叫“蓝翔WA掘机”的，笑不活了:)
+            return -1; 
         }
     }
 
-    printf("All Correct!\n");  // 还有个队名叫“AC自动机”的，和隔壁“WAWA大哭”对标是吧:)
+    printf("All Correct!\n"); 
+    cudaFree(pcounter);
     return 0;
 }
